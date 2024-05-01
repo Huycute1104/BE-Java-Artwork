b18bcc3d2b93f67043f89bc1331a4c62aeed9689
Huy Pham <huypt110402@gmail.com>
2024-05-01 13:51:51 +0700

    base

---
+++ src/main/java/com/example/artworksharing/ArtWorkSharingApplication.java
@@ -0,0 +1,13 @@
+package com.example.artworksharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtWorkSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtWorkSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/artworksharing/ArtWorkSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artworksharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtWorkSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
